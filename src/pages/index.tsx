8bc440ee69cd68a22f16f52cf7161a787191c7a3
Anders Gustafsson <andersgee@gmail.com>
2022-11-26 22:02:15 +0100

    tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,34 @@ const Page: NextPage = () => {
       <Link className="block" href="/ad/JP9d7">
         ad/JP9d7
       </Link>
+
+      <div className="flex flex-col gap-2">
+        <Link href="/example">go to example</Link>
+        <h1>testing caching and page load time... for user pages</h1>
+        <h2 className="mt-4">existing pregenerated</h2>
+        <Link href="/eu">go to eu</Link>
+        <Link href="/aliqua">go to aliqua</Link>
+        <Link href="/tempor">go to tempor</Link>
+        <Link href="/ullamco">go to ullamco</Link>
+        <Link href="/mollit">go to mollit</Link>
+        <Link href="/laboris">go to laboris</Link>
+
+        <h2 className="mt-4">existing but not pregenerated</h2>
+        <Link href="/deserunt">go to deserunt</Link>
+        <Link href="/magna">go to magna</Link>
+        <Link href="/commodo">go to commodo</Link>
+        <Link href="/ad">go to ad</Link>
+
+        <h2 className="mt-4">nonexisting</h2>
+        <Link href="/kekker">go to kekker</Link>
+        <Link href="/berkor">go to berkor</Link>
+        <Link href="/mamma">go to mamma</Link>
+
+        <h2 className="mt-4">nonexisting generated anyway?..</h2>
+        <Link href="/kekker">go to kekker</Link>
+        <Link href="/berkor">go to berkor</Link>
+        <Link href="/mamma">go to mamma</Link>
+      </div>
     </div>
   );
 };
